889d97e2ca5d745922a4d4aeeec9adfd5c699bf4
K4l3b <143396031+CalebDevX@users.noreply.github.com>
2025-05-21 16:46:54 +0100

    settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'K4l3b Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "K4l3b Bot",
+  botOwner: 'kleb', // Your name
+  ownerNumber: '+2348096567473', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
